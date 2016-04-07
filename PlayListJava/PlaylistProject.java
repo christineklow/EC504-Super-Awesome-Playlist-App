e88a32aae60ac7029c689c07f1a4a1e07c077e8e
anero17 <anero17@bu.edu>
2016-04-07 14:34:10 -0400

    chnages

--- PlayListJava/PlaylistProject.java
+++ PlayListJava/PlaylistProject.java
@@ -1,5 +1,3 @@
-
-  
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -19,70 +17,77 @@ public class PlaylistProject{
         
         
         //label
-        JLabel jlbempty = new JLabel("Enter PlayList?");
-        jlbempty.setPreferredSize(new Dimension(100, 100));
+        JLabel jlbempty = new JLabel("Awesome Playlist App");
+        jlbempty.setForeground(Color.WHITE);
+        jlbempty.setFont(new Font("Monaco", Font.BOLD, 40));
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(0,25,40,0);
+        c.gridwidth = 3;
         c.weightx = 0.5;
         c.gridx = 0;
         c.gridy = 0;
         panel.add(jlbempty, c);
         
-        JLabel label = new JLabel("");
-        label.setPreferredSize(new Dimension(100, 100));
+        c.fill = GridBagConstraints.NONE;
+        c.insets = new Insets(0,0,0,0);
+        
+        JLabel label = new JLabel("Enter Playlist: ");
+        label.setFont(new Font("Monaco", Font.PLAIN, 24));
+        label.setForeground(Color.WHITE);
+        c.gridwidth = 1;
         c.weightx = 0.5;
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 1;
         panel.add(label, c);
         
         //empty text field
         JTextField t1 = new JTextField(10);
+        c.gridwidth = 1;
         c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 1;
+        c.gridx = 1;
+        c.gridy = 2;
         panel.add(t1, c);
         
-        JTextField t2 = new JTextField(10);
-        c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 2;
-        panel.add(t2, c);
         
         
         //Radio Buttons
         ButtonGroup buttonGroup = new ButtonGroup();
         JRadioButton y = new JRadioButton("Text File");
         y.setBackground(aColor);
+        y.setForeground(Color.WHITE);
+        y.setFont(new Font("Monaco", Font.PLAIN, 18));
         buttonGroup.add(y);
+        y.putClientProperty("JComponent.sizeVariant", "large");
+        c.gridwidth = 1;
         c.weightx = 0.5;
-        c.gridx = 0;
+        c.gridx = 1;
         c.gridy = 1;
         panel.add(y, c);
         JRadioButton n = new JRadioButton("Manual Input");
+        n.setFont(new Font("Monaco", Font.PLAIN, 18));
         n.setBackground(aColor);
+        n.setForeground(Color.WHITE);
         buttonGroup.add(n);
+        c.gridwidth = 1;
         c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 2;
+        c.gridx = 2;
+        c.gridy = 1;
         panel.add(n, c);
         
         //two buttons
-        JButton startButton = new JButton("Enter");
-        startButton.setPreferredSize(new Dimension(100, 50));
+        JButton startButton = new JButton("Submit");
+        startButton.setPreferredSize(new Dimension(100, 25));
+        c.gridwidth = 1;
         c.weightx = 0.5;
-        c.gridx = 1;
-        c.gridy = 1;
-        panel.add(startButton, c);
-        
-        JButton startButton2 = new JButton(";)");
-        startButton2.setPreferredSize(new Dimension(100, 50));
-        c.weightx = 0.5;
-        c.gridx = 1;
+        c.gridx = 2;
         c.gridy = 2;
-        panel.add(startButton2, c);
+        panel.add(startButton, c);
        
         panel.setBackground(aColor);
         //frame to encapsulate panel and pack components in
         JFrame frame = new JFrame("PlayList Project");
-        frame.addWindowListener(new WindowAdapter() {
+        frame.addWindowListener(new WindowAdapter() 
+        {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
@@ -93,27 +98,19 @@ public class PlaylistProject{
         
         
         
-        startButton.addActionListener(new ActionListener() {
+        startButton.addActionListener(new ActionListener() 
+        {
  
             public void actionPerformed(ActionEvent e)
             {
                 //Execute when button is pressed
-                System.out.println("You clicked the button");
-                String text = t1.getText();
-                t1.setText("");
-                label.setText(text);
-                panel.revalidate();
+                //String text = t1.getText();
+                //t1.setText("");
+                //label.setText(text);
+                //panel.revalidate();
             }
         });
         
-        startButton2.addActionListener(new ActionListener() {
- 
-            public void actionPerformed(ActionEvent e)
-            {
-                //Execute when button is pressed
-                System.out.println("You clicked the other button");
-            }
-        });
     }
     
     
