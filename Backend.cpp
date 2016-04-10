19e7d5b815121d971032cb07d5b49883eff78572
christineklow <clow@bu.edu>
2016-04-10 18:53:53 -0400

    backend: list top 8 playlist song appears

--- Backend.cpp
+++ Backend.cpp
@@ -10,13 +10,15 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <utility>
+#include <unordered_map>
 
 using namespace std;
 
 /** Goals/Requirements:
 
 	Able to fetch playlists by included song based on playlist popularity
-		Playlist object with attribute songList, Popularity
+		Playlist object with attribute song list, Popularity
 
 	Able to display the most popular playlists
 		Use data struct with most popular on top?
@@ -32,25 +34,47 @@ using namespace std;
 	Use C++ to minimize runtime?
 **/
 
-/** IMPLEMENTATION
-	Data Structure Ideas: Binary Search Tree, hash for playlist
-	string for playlist name
+struct songData {
+        songData() : songID() {}
+        songData(string newID)
+         : songID(newID) {}
 
-	class for song, playlist
+        string songID;
+        int popularity;
+        /*
+        map<string,int> playlist;
 
-	functions
-	input manual
-	input from text filename 
+        // list playlists song is in
+        void listPlaylist(){
+        	for( map<string,int>::iterator it = playlist.begin() ; it != playlist.end(); ++it)
+        		cout<< it -> first << " "<< it -> second << endl;
+        }
 
-	error check is actual song
+        void removePlaylist(string playlistName){
+        	map<string,int>::iterator it = playlist.find(playlistName);
+        	if(it != playlist.end()){
+        		playlist.erase(it);		// if playlist exists in song's database, erase it
+        		popularity--;			// decrement song's popularity
+        	}
+        }
 
-	display 8 most popular playlist
-	remove least popular playlist
+        void addPlaylist(string playlistName, int playlistPopularity){
 
-	given song, display most popular playlist
-**/
+        	// if playlist already exists, update playlist popularity
+        	map<string,int>::iterator it = playlist.find(playlistName);
+        	if(it != playlist.end()){
+        		it -> second += playlistPopularity;
+        	}
+        	else{	// if playlist doesn't exist, add to song's playlist database
+        		playlist[playlistName] = playlistPopularity;
+        		popularity++;	// update song popularity
+        	}
+        }*/
+};
 
-map<string,string> songList;
+map<string,songData> songList;			// database of songs with the songID and popularity
+unordered_map<string,int> playlistDB;	// database of playlists with the popularity
+map<int,string> popularityDB;			// databse of playlists sorted by popularity
 
 void loadSongs(){
 	// load songs from song_list.txt into datastructure songList
@@ -68,11 +92,11 @@ void loadSongs(){
 		getline (file,tempID,'\t');
 		getline (file,tempSong,'\t');
 		getline (file,tempArtist,'\n');
-		songList[tempSong] = tempID;
+		songList[tempSong] = songData(tempID);
 	}
 
-	for ( map<string,string>::iterator it= songList.begin(); it!=songList.end(); ++it)
-    cout << "SONG NAME: " << it->first << "     SONG ID: " << it->second << endl;
+	for ( map<string,songData>::iterator it= songList.begin(); it!=songList.end(); ++it)
+    cout << "SONG NAME: " << it->first << "     SONG ID: " << it->second.songID << endl;
 
 	cout<<"...\nSongs loaded."<<endl;
 
@@ -80,19 +104,18 @@ void loadSongs(){
 
 }
 
-// returns ID of song if it exists in database
-string getSongID(string searchName){
+// returns iterator of song if it exists in database
+map<string,songData>::iterator getSong(string searchName){
 
 	cout<< "\nSearching for: " << searchName <<endl;
 
-	map<string,string>::iterator it;
+	map<string,songData>::iterator it;
 	it = songList.find(searchName);		// Search for song in database
 	
 	if (it != songList.end()){			// if song exists in database
-    	cout << "SONG NAME: " << it->first << "     SONG ID: " << it->second << endl;
-    	return it -> second;
+    	cout << "SONG NAME: " << it->first << "     SONG ID: " << it->second.songID << endl;
     }
-	else return "-1"; // Song does not exist
+	return it;
 
 }
 
@@ -100,14 +123,66 @@ string getSongID(string searchName){
 void getSongPlaylist(string searchName){
 
 	// first obtain song id number if it exists
-	string songID = getSongID(searchName);
+	map<string,songData>::iterator songSearch = getSong(searchName);
 
-	if(songID != "-1"){
-		// lists top 8 playlists song is in
+	if(songSearch != songList.end()){ 	// song exists in database
+
+		map<int,string> songPlaylists;	// list of playlists that contains the specific song
+		int songPopularity = 0;
+
+		// search playlist database for playlists that contains song
+		for( unordered_map<string, int>:: iterator it = playlistDB.begin(); it != playlistDB.end(); ++it){
+
+			// if the playlist contains the song
+			if(it-> first.find( songSearch -> second.songID) != string::npos){
+				songPlaylists[it -> second] = it -> first;	// add playlist to songPlaylists sorted automatically by popularity
+				songPopularity++; 							// increment song popularity based on how many playlists its in
+			}
+		}
+
+		songSearch -> second.popularity = songPopularity;	// update song popularity
+
+		// list top 8 playlists
 		cout<<"\nTop 8 Playlists: "<<endl;
+		map<int, string>:: iterator it = songPlaylists.end();
+		for( int count = 0; count < 8 && it != songPlaylists.begin() ; count++){
+			it--;
+			cout<< it -> second << ": " << it -> first <<endl;
+		}
 	}
 	else cout<<"Song does not exist in database"<<endl;
 }
+
+void removeLeastPopular(){
+	if( playlistDB.size() > 1024 ){
+		//look for least popular and remove least popular
+
+	}
+}
+
+// adds playlist to database
+void addPlaylist(string playlistInput){
+
+	string playlistSongs, temp_popularity;
+	int popularity;
+
+	// playlist input must be in the format <Playlist \t Popularity(integer)>
+	stringstream ss(playlistInput);
+	getline(ss,playlistSongs,'\t');					// get playlist songs from input
+	getline(ss, temp_popularity, '\n');				// get popularity
+	popularity = stoi(temp_popularity,nullptr,10); 	// convert popularity from string to in
+
+	// check to see if playlist has been added already
+	unordered_map<string, int>:: iterator it = playlistDB.find(playlistSongs);
+	if( it != playlistDB.end() ){					// playlist is already in database
+		it -> second = it -> second + popularity;	// update popularity of playlist
+	}
+	else playlistDB[playlistSongs] = popularity; 	// if it does not exist yet, add it
+
+	// check to see if there are more than 1024 playlists, if so remove least popular
+	removeLeastPopular();
+}
+
 
 int main(){
 
@@ -117,11 +192,30 @@ int main(){
 	catch(const char* msg){ // Error in opening file
 		cerr << msg << endl;
 	}
-	
+
+
+	// generating test playlists
+	playlistDB["1 2 3 4"] = 1;
+	playlistDB["4 5 6"] = 2;
+	playlistDB["2 5 6"] = 3;
+	playlistDB["1 4 6"] = 4;
+	playlistDB["7 4 8"] = 5;
+	playlistDB["4 5 9"] = 6;
+	playlistDB["4 5 10"] = 7;
+	playlistDB["4 8 6"] = 8;
+	playlistDB["3 4 6"] = 9;
+	playlistDB["7 5 4"] = 10;
+	playlistDB["1 5 4"] = 11;
+	playlistDB["3 4 9"] = 12;
+	playlistDB["4 9 6"] = 13;
+	playlistDB["4 5 6"] = 14;
+
 	string searchName;
 	cout << "\nEnter Song to Search: ";
 	getline(std::cin,searchName);
 	getSongPlaylist(searchName);
+
+	//addPlaylist(searchName);
 
 	return 0;
 }
