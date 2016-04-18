d4e5ea2ea754dc5ad550609ef14aeed7d88fa3ee
anero17 <anero17@bu.edu>
2016-04-18 15:48:59 -0400

    gui changes

--- PlayListJava/PlaylistProject.java
+++ PlayListJava/PlaylistProject.java
@@ -3,29 +3,30 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
-import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.File;
 
 public class PlaylistProject{
-
-   /*public ArrayList executeCommand(String command){
+    //going to the backend
+   public ArrayList executeCommand(String command){
        String s;
+       ArrayList dataList = new ArrayList();
        try {
            Runtime rt = Runtime.getRuntime();
-           Process pr = rt.exec("./a.out");
+           Process pr = rt.exec(command);
            BufferedReader br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
            while ((s= br.readLine()) != null)
-                System.out.println(s);
-                pr.waitFor();
-                //System.out.println("exit: "+ pr.exitValue());
-                pr.destroy();
+                dataList.add(s);
+           pr.waitFor();
+           pr.destroy();
        }
        catch (Exception e) {
            System.out.println(e.toString());
            e.printStackTrace();
-	   }
-   }*/
+        }
+       return dataList;
+   }
     
     
    public static void main(String[] args) {
@@ -42,6 +43,9 @@ public class PlaylistProject{
         JPanel SearchPanel = new JPanel(new GridBagLayout());
         GridBagConstraints cSearch = new GridBagConstraints();
         
+        JPanel PopularPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints cPop = new GridBagConstraints();
+        
         
         //label
         JLabel jlbempty = new JLabel("\u266B Awesome Playlist App \u266B");
@@ -50,7 +54,7 @@ public class PlaylistProject{
         TitlePanel.add(jlbempty, cTitle);
         
         
-        //enter laylist tab
+        //enter playlist tab
         JLabel label = new JLabel("Enter Playlist");
         label.setFont(new Font("Monospaced", Font.BOLD, 22));
         label.setForeground(Color.WHITE);
@@ -96,6 +100,11 @@ public class PlaylistProject{
         c.insets = new Insets(0,40,5,40);
         PlayListPanel.add(t1, c);
         
+        //browse button
+        JButton browseButton = new JButton("Browse");
+        browseButton.setFont(new Font("Lucida", Font.PLAIN, 14));
+        
+        
         //submit button
         JButton startButton = new JButton("Submit");
         startButton.setFont(new Font("Lucida", Font.PLAIN, 18));
@@ -114,8 +123,77 @@ public class PlaylistProject{
         c.gridx = 1;
         c.gridy = 4;
         PlayListPanel.add(playlistLabel, c);
+                
+        
+        //popular playlist tab
+        JLabel titlePop = new JLabel("Top Eight Playlists");
+        titlePop.setFont(new Font("Monospaced", Font.BOLD, 30));
+        titlePop.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 0;
+        cPop.insets = new Insets(0,0,30,0);
+        PopularPanel.add(titlePop, cPop);
+        
+        //enter command here to get the top eight playlists
+        //replace word "playlist"
         
         
+        JLabel playlist1 = new JLabel("playlist");
+        playlist1.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist1.setForeground(Color.WHITE);
+        cPop.insets = new Insets(0,0,0,0);
+        cPop.gridx = 0;
+        cPop.gridy = 1;
+        PopularPanel.add(playlist1, cPop);
+        
+        JLabel playlist2 = new JLabel("playlist");
+        playlist2.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist2.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 2;
+        PopularPanel.add(playlist2, cPop);
+        
+        JLabel playlist3 = new JLabel("playlist");
+        playlist3.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist3.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 3;
+        PopularPanel.add(playlist3, cPop);
+        
+        JLabel playlist4 = new JLabel("playlist");
+        playlist4.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist4.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 4;
+        PopularPanel.add(playlist4, cPop);
+        
+        JLabel playlist5 = new JLabel("playlist");
+        playlist5.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist5.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 5;
+        PopularPanel.add(playlist5, cPop);
+        
+        JLabel playlist6 = new JLabel("playlist");
+        playlist6.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist6.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 6;
+        PopularPanel.add(playlist6, cPop);
+        
+        JLabel playlist7 = new JLabel("playlist");
+        playlist7.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist7.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 7;
+        PopularPanel.add(playlist7, cPop);
+        
+        JLabel playlist8 = new JLabel("playlist");
+        playlist8.setFont(new Font("Monospaced", Font.BOLD, 22));
+        playlist8.setForeground(Color.WHITE);
+        cPop.gridx = 0;
+        cPop.gridy = 8;
+        PopularPanel.add(playlist8, cPop);
         
         //search song tab:
         JLabel esonglabel = new JLabel(" Enter Song");
@@ -141,22 +219,25 @@ public class PlaylistProject{
         
         
         DefaultListModel listModel = new DefaultListModel();
+        //initialize should be blank
         listModel.addElement("Love Hurts");
         listModel.addElement("Love Letter");
         listModel.addElement("Love Song");
         listModel.addElement("Love Me Do");
         JList list = new JList(listModel);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setFont(new Font("Monospaced", Font.PLAIN, 16));
         list.setSelectedIndex(0);
         list.setVisibleRowCount(4);
         JScrollPane listScrollPane = new JScrollPane(list);
+        listScrollPane.setViewportView(list);
         cSearch.gridwidth = 3;
         cSearch.gridheight = 4;
         cSearch.weightx = 0.5;
         cSearch.insets = new Insets(0,40,30,40);
         cSearch.gridx = 0;
         cSearch.gridy = 2;
-        //SearchPanel.add(listScrollPane, cSearch);
+        SearchPanel.add(listScrollPane, cSearch);
         
         
         //submit button
@@ -181,14 +262,17 @@ public class PlaylistProject{
         SearchPanel.add(searchLabel, cSearch);
       
         PlayListPanel.setBackground(aColor);
+        PopularPanel.setBackground(aColor);
         TitlePanel.setBackground(aColor);
         SearchPanel.setBackground(aColor);
         PlayListPanel.setBorder(BorderFactory.createLineBorder(Color.WHITE, 3));
         SearchPanel.setBorder(BorderFactory.createLineBorder(Color.WHITE, 3));
+        PopularPanel.setBorder(BorderFactory.createLineBorder(Color.WHITE, 3));
         
         JTabbedPane categories = new JTabbedPane();
         categories.setFont(new Font("Monospaced", Font.PLAIN, 20) );
         categories.addTab("Enter Playlist", PlayListPanel);
+        categories.addTab("Top Playlists", PopularPanel);
         categories.addTab("Search Song", SearchPanel);
         
         categories.setForeground(aColor);
@@ -219,6 +303,32 @@ public class PlaylistProject{
         {
             public void actionPerformed(ActionEvent e)
             {
+                String textfield1 = t1.getText();
+                if (y.isSelected()){
+                    if (textfield1.length() == 0)
+                        playlistLabel.setText("Input A Valid File!");
+                    else{
+                        t1.setText(""); //check if this is okay
+                        //call on input playlist functions here!
+                        playlistLabel.setText("Playlist File Added!");
+                        //update top playlist 
+                    }
+                }
+                else if (n.isSelected()){
+                    if (textfield1.length() == 0)
+                        playlistLabel.setText("Input A Valid File!");
+                    else{
+                        t1.setText("");//check if this is okay
+                        //call on input playlist functions here
+                        playlistLabel.setText("Playlist Added!");
+                        //update top playlists
+                    }
+                }
+                else{
+                    playlistLabel.setText("Choose A Method!");
+                }
+                //frame.pack();
+                frame.validate();
             }
         });
         
@@ -228,30 +338,107 @@ public class PlaylistProject{
             {
                 //search Results
                 String textfield2 = t2.getText();
-                if (textfield2.length() != 0){
-                    
+                if (textfield2.length() != 0){                  
                     searchLabel.setText("Search Results");
+                    //add most popular song or did you mean?
+                    list.setVisibleRowCount(0);
+                    listModel.removeAllElements();
+                }
+                else{
+                    searchLabel.setText("Input A Valid Song!");
+                    list.setVisibleRowCount(0);
+                    listModel.removeAllElements();
                 }
                 t2.setText("");
+                frame.revalidate();
             }
         
         });
         
-        t2.getDocument().addDocumentListener(new DocumentListener()
+        browseButton.addActionListener(new ActionListener()
         {
-            public void changedUpdate(DocumentEvent e)
+            public void actionPerformed(ActionEvent e)
             {
+                JFileChooser fc = new JFileChooser();   
+                fc.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int returnVal = fc.showOpenDialog(frame);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    // user selects a file
+                    File selectedFile = fc.getSelectedFile();
+                    t1.setText(selectedFile.getAbsolutePath());
+                }
+            }
+        
+        });
+        
+        
+        Timer timer = new Timer(75, new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                //change and make boxes visible here
+                String textfield2 = t2.getText();
+                if(textfield2.length() != 0){
+                    listModel.removeAllElements();
+                    //add elements from command line call here                       
+                    //set visibility to the necessary amount here
+                    list.setVisibleRowCount(3);
+                    listModel.addElement("hello");
+                    listModel.addElement("hi");
+                    listModel.addElement(t2.getText());
+                    frame.revalidate();
+                }
 
             }
-
-            public void insertUpdate(DocumentEvent e) 
-            {
-
+        });
+        timer.start();
+        
+        
+        //to select the proper autocomplete option
+        MouseListener mouseListener = new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                String selectedItem = (String) list.getSelectedValue();
+                t2.setText(selectedItem);
             }
-
-            public void removeUpdate(DocumentEvent e)
-            {
-
+        };
+        list.addMouseListener(mouseListener);
+        
+        //radio button listeners for browsing files and formatting 
+        y.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                GridBagConstraints cBrowse = new GridBagConstraints();
+                cBrowse.fill = GridBagConstraints.NONE;
+                cBrowse.insets = new Insets(0,0,7,20);
+                cBrowse.gridwidth = 1;
+                cBrowse.gridx = 4;
+                cBrowse.gridy = 2;
+                PlayListPanel.add(browseButton, cBrowse);
+                
+                
+                //move the label over, since it gets cut off with the browse button
+                PlayListPanel.remove(playlistLabel);
+                GridBagConstraints ctemp = new GridBagConstraints();
+                ctemp.insets = new Insets(0,0,0,150);
+                ctemp.gridwidth = 2;
+                ctemp.weightx = 0.5;
+                ctemp.gridx = 1;
+                ctemp.gridy = 4;
+                PlayListPanel.add(playlistLabel, ctemp);
+                
+                frame.repaint();                
+                frame.revalidate();
+            }
+        });
+        n.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                PlayListPanel.remove(browseButton);
+                PlayListPanel.remove(playlistLabel);
+                GridBagConstraints ctemp = new GridBagConstraints();
+                ctemp.insets = new Insets(0,0,0,0);
+                ctemp.gridwidth = 1;
+                ctemp.weightx = 0.5;
+                ctemp.gridx = 1;
+                ctemp.gridy = 4;
+                PlayListPanel.add(playlistLabel, ctemp);
+                frame.repaint();
             }
         });
     }
