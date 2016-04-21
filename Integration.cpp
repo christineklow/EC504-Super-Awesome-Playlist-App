133a80a10d87efe6dc3c59d26c7cbdbcc828aabb
Shane McCormack <aonanam@bu.edu>
2016-04-21 02:13:23 -0400

    updating integration

--- Integration.cpp
+++ Integration.cpp
@@ -3,14 +3,30 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <stdio.h>
+
 
 using namespace std;
+
+int main( int argc, char *argv[] )
+{
+  string argString = str(argv);
+  argString.erase(argString.begin(), argString.begin()+8)   #stripping out arg[0]
+  interpretCommand(argString);
+  return;
+}
 
 void interpretCommand(string commandString)
 {
   istringstream iss(commandString);
   char codeParam;
+  if (codeParam == 's')
+  {
+    loadSongs();
+    return;
+  }
   iss >> codeParam;
+  loadPlaylistData();
   commandString.erase(commandString.begin(), commandString.begin()+2)
   switch(codeParam)
   {
@@ -21,14 +37,31 @@ void interpretCommand(string commandString)
       importPlaylists(commandString)
       break;
     case 't' :
-      
-
-
+      findSongPrefixes(commandString)
+      break;
+    case 'p' :
+      getSongPlaylist(commandString)
+      break;
+    case 'l' :
+      mostPopularPlaylist()
+      break;
+    default case :
+      cout << "You done fucked up, son" << endl;
   }
-
-
-
-  for
-
-
 }
+
+/*
+
+int main( int argc, char *argv[] )  {
+
+   if( argc == 2 ) {
+      printf("The argument supplied is %s\n", argv[1]);
+   }
+   else if( argc > 2 ) {
+      printf("Too many arguments supplied.\n");
+   }
+   else {
+      printf("One argument expected.\n");
+   }
+}
+*/
